cbee714d47bcbcee85ade5aa2bfe1765b81ee5cc
“liambrockpy” <liambrock72@gmail.com>
2023-01-16 14:12:38 +0000

    Show instructions and high score modals

---
+++ frontendreact/vite-project/src/components/HighScoresList/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const URL = "";
+
+const HighScoreList = () => {
+    // const data = await axios.get(URL);
+    const data = [
+        {
+            id: 1,
+            name: "John",
+            score: 100
+        },
+        {
+            id: 2,
+            name: "Jane",
+            score: 200
+        }
+    ];
+
+    return (
+        <table>
+            <tr>
+                <th>Player Name</th>
+                <th>Score</th>
+            </tr>
+            {data.map((item) => (
+                <tr key={item.id}>
+                    <td>{item.name}</td>
+                    <td>{item.score}</td>
+                </tr>
+            ))}
+        </table>
+    )
+}
+
+export default HighScoreList;
--- frontendreact/vite-project/src/pages/home/index.jsx
+++ frontendreact/vite-project/src/pages/home/index.jsx
@@ -1,7 +1,19 @@
 import React from "react"
 
+import MyModal from "../../components/ModalButton"
+import Instructions from "../../components/Instructions"
+import HighScoreList from "../../components/HighScoresList"
+
 const HomePage = () => {
-	return <h1>Welcome</h1>
+	return (
+	<>
+		<h1>Home Page</h1>
+		<MyModal Component={<Instructions/>} ButtonText="Instructions"/>
+		<MyModal Component={<HighScoreList/>} ButtonText="High Scores"/>
+	</>
+	
+		
+	)
 }
 
 export default HomePage
