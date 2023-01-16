7269ff844405d1cf2a145515eb5a7fbb96dbeb8c
Mizna Uppal <miznauppal@gmail.com>
2023-01-16 14:51:21 +0000

    done

--- frontendreact/vite-project/src/main.jsx
+++ frontendreact/vite-project/src/main.jsx
@@ -2,9 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import HomePage from './pages/home';
+import GamePage from './pages/game';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/game",
+    element: <GamePage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ frontendreact/vite-project/src/pages/game/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const game = () => {
+
+    return (
+        <>
+            <h1>Game</h1>
+        </>
+    )
+}
+
+export default game;
