13326fd7f6aefbd2e11c9c7464b849bb7510561e
“liambrockpy” <liambrock72@gmail.com>
2023-01-16 10:09:53 +0000

    Initial routing
    Co-authored-by: Adam-Dowsett <Adam-Dowsett@users.noreply.github.com>

--- frontendreact/vite-project/src/main.jsx
+++ frontendreact/vite-project/src/main.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import "./index.css"
+import {
+	createBrowserRouter,
+	RouterProvider,
+	Route,
+	Link,
+} from "react-router-dom"
+import HomePage from "./pages/home"
+import Game from './pages/game'
+import Winner from './pages/winner'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <HomePage />,
+	},
+	{
+		path: "game",
+		element: <Game />,
+	},
+	{
+		path: "winner",
+		element: <Winner />,
+	},
+])
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 )
---
+++ frontendreact/vite-project/src/pages/winner/index.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Winner = () => {
+    
+	return (
+		<div>
+			<h1>Winner is Person </h1>
+		</div>
+	)
+}
+
+export default Winner
