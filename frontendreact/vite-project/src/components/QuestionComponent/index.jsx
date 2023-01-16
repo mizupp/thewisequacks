9b21042eff741b029a4adbaaa39998f703cc2aa4
“liambrockpy” <liambrock72@gmail.com>
2023-01-16 17:44:51 +0000

    Fix key warning

--- frontendreact/vite-project/src/components/QuestionComponent/index.jsx
+++ frontendreact/vite-project/src/components/QuestionComponent/index.jsx
@@ -8,7 +8,7 @@ const QComp = ({data, onClose}) => {
     return (
         <>
             <h2>{data.question}</h2>
-            {shuffledAnswers.map(a => <p>{a}</p>)}
+            {shuffledAnswers.map((a, i) => <p key={i}>{a}</p>)}
             <button onClick={onClose}>Close</button>
         </>
     )
