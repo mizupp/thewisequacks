103e20b9fa38bd3b4c3335686d348802b0b6d5be
“liambrockpy” <liambrock72@gmail.com>
2023-01-16 14:08:44 +0000

    Set up routing through consistent layout

--- frontendreact/vite-project/src/main.jsx
+++ frontendreact/vite-project/src/main.jsx
@@ -3,32 +3,13 @@ import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
 import {
-	createBrowserRouter,
-	RouterProvider,
-	Route,
-	Link,
+	BrowserRouter,
 } from "react-router-dom"
-import HomePage from "./pages/home"
-import Game from './pages/game'
-import Winner from './pages/winner'
-
-const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <HomePage />,
-	},
-	{
-		path: "game",
-		element: <Game />,
-	},
-	{
-		path: "winner",
-		element: <Winner />,
-	},
-])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
 	</React.StrictMode>
 )
