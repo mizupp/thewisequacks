9cdacfc055636935adda975e6840224c6099fc49
“liambrockpy” <liambrock72@gmail.com>
2023-01-17 18:03:42 +0000

    Update

--- frontendreact/vite-project/src/components/WaitingRoom/index.jsx
+++ frontendreact/vite-project/src/components/WaitingRoom/index.jsx
@@ -1,49 +1,50 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Navigate } from 'react-router-dom';
-import { startGame } from '../../actions';
-import { getState } from '../../actions';
-import './style.css';
+import React, { useEffect } from "react"
+import { useSelector, useDispatch } from "react-redux"
+import { Navigate } from "react-router-dom"
+import { startGame } from "../../actions"
+import { getState } from "../../actions"
+import "./style.css"
 
 const WaitingRoom = () => {
-    const data = useSelector(state => state.gameState);
-    const user = useSelector(state => state.user);
-    const socket = useSelector(state => state.socket);
-    const dispatch = useDispatch()
-    console.log(data);
+	const data = useSelector((state) => state.gameState)
+	const user = useSelector((state) => state.user)
+	const socket = useSelector((state) => state.socket)
+	const dispatch = useDispatch()
+	console.log(data)
 
-    useEffect(() => {
-        getState(socket)
-    },[])
+	// useEffect(() => {
+	//     getState(socket)
+	// },[])
 
-    function handleButtonClick(){
-        dispatch(startGame());
-        let newState = {
-            ...data,
-            isGameStarted: true
-        }
-        
-        socket.emit('send state to players', newState )
-        console.log(data);
-    }
+	function handleButtonClick() {
+		dispatch(startGame())
+		let newState = {
+			...data,
+			isGameStarted: true,
+		}
 
-    return (
-        <>
-        <div>
-            <div className="waiting-message">
-                <h1>This is {data.host}'s room </h1>
-                <h2>Please wait for other players...</h2>
-            </div>
-            <p>Share room name to friends:</p>
-            <div>
-                <span>{data.roomName}</span>
-            </div>
-            <button onClick={handleButtonClick} className="btn start-game">START GAME</button>
-            {data.isGameStarted && <Navigate to='/game' />}
-        </div>
-        </>
+		socket.emit("send state to players", newState)
+		console.log(data)
+	}
 
-    );
+	return (
+		<>
+			<div>
+				<div className="waiting-message">
+					<h1>This is {data.host}'s room </h1>
+					<h2>Please wait for other players...</h2>
+				</div>
+				<p>Share room name to friends:</p>
+				<div>
+					<span>{data.roomName}</span>
+				</div>
+				<button onClick={handleButtonClick} className="btn start-game">
+					START GAME
+				</button>
+				{data.isGameStarted && <Navigate to="/game" />}
+			</div>
+		</>
+	)
 }
 
-export default WaitingRoom;
+export default WaitingRoom
--- frontendreact/vite-project/src/components/startGame/index.jsx
+++ frontendreact/vite-project/src/components/startGame/index.jsx
@@ -1,54 +1,78 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from "react-redux";
-import { storeSocket, changeState, storeUser, addUser, updateScore, setCompleted } from "../../actions";
-import axios from "axios";
-import { useNavigate } from 'react-router-dom';
-import { Link } from "react-router-dom"; 
-
+import React, { useState } from "react"
+import { useSelector, useDispatch } from "react-redux"
+import {
+	storeSocket,
+	changeState,
+	storeUser,
+	addUser,
+	updateScore,
+	setCompleted,
+} from "../../actions"
+import axios from "axios"
+import { useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom"
+import "./style.css"
 const StartGame = () => {
-    const navigate = useNavigate()
-    const dispatch = useDispatch();
-    const socket = useSelector((state) => state.socket);
-    const [username, setName] = useState("");
-    const [room, setRoom] = useState("")
+	const navigate = useNavigate()
+	const dispatch = useDispatch()
+	const socket = useSelector((state) => state.socket)
+	const [username, setName] = useState("")
+	const [room, setRoom] = useState("")
 
-    const data = useSelector(state => state.gameState);
+	const data = useSelector((state) => state.gameState)
 
-    const handleCreate = () => {
-        const playerInfo = {
-            userID: socket.id,
-            name: username,
-            isHost: true,
-            score: 0
-        }
-        console.log(playerInfo)
-        socket.emit('create game', playerInfo);
-        dispatch(addUser(playerInfo))
-        navigate('/lobby')
-    }
+	const handleCreate = () => {
+		const playerInfo = {
+			userID: socket.id,
+			name: username,
+			isHost: true,
+			score: 0,
+		}
+		console.log(playerInfo)
+		socket.emit("create game", playerInfo)
+		dispatch(addUser(playerInfo))
+	}
 
-    const handleJoin = () => {
-        const playerInfo = {
-            userID: socket.id,
-            name: username,
-            isHost: false,
-            score: 0
-        }
-        socket.emit('join game', {room, playerInfo})
-       dispatch(addUser(playerInfo))
-       navigate('/lobby')
-    }
+	const handleJoin = () => {
+		const playerInfo = {
+			userID: socket.id,
+			name: username,
+			isHost: false,
+			score: 0,
+		}
+		socket.emit("join game", { room, playerInfo })
+		dispatch(addUser(playerInfo))
+	}
 
-    return(
-        <div className="start-game">
-            <input type="text" name="username" value={username} placeholder="name" onChange={(e) => setName(e.target.value)}/>
-            <input type="text" name="room" value={room} placeholder="room code" onChange={(e) => setRoom(e.target.value)}/>
-            <br />
-            <button onClick={handleJoin}>Join game</button>
-                        
-            <Link onClick={handleCreate} to="/lobby" className="btn btn-primary">Create game</Link>
-        </div>
-    )
+	return (
+		<div className="flex flex-col">
+			<input
+				className="enterName"
+				type="text"
+				name="username"
+				value={username}
+				placeholder="name"
+				onChange={(e) => setName(e.target.value)}
+			/>
+			<input
+				type="text"
+				name="room"
+				value={room}
+				placeholder="room code"
+				onChange={(e) => setRoom(e.target.value)}
+			/>
+
+			<div>
+				<button className="joincreate" onClick={handleJoin}>
+					Join game
+				</button>
+
+				<button className="joincreate" onClick={handleCreate}>
+					Create game
+				</button>
+			</div>
+		</div>
+	)
 }
 
-export default StartGame;
+export default StartGame
